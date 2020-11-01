1df1048ef188126299c443eb13c441cb07d63b81
Nguyen Dai <ngtrdai290197@gmail.com>
2020-11-01 22:10:21 +0700

    chore(): add Dockerfile | test connect to server

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const SAY_HI = gql`
+  query sayHi {
+    sayHi
+  }
+`;
 
 function App() {
+  const { data, loading } = useQuery(SAY_HI);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {!loading &&  data.sayHi}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
